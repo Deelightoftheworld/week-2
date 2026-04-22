129f1ead040835dcabcebc77ffd7d58583a84b55
Deelightoftheworld <shantelhaddasah@gmail.com>
2026-04-22 09:02:58 +0100

    End of day 7

--- script.js
+++ script.js
@@ -57,3 +57,58 @@ function checker() {
 
 
 checker()
+
+
+function  checkerWithSwitch() {
+    //  1.Get score from user
+    let score = prompt ("Please enter a score between 0 and 100")
+    score = parseFloat(score)
+
+    //  2.Check if score entered is valid
+    let isValid;
+    if(score >= 0 && score <= 100){
+       isValid = true
+    } else {
+        isValid = false
+    }  
+
+     //  3.Grade based on score
+    if (isValid) {
+   switch(true) {
+    case (score >= 70):
+        alert("Congratulations, you got an A")
+        break;
+
+        case (score >= 60 && score < 70):
+            alert ("Good job, you got a B")
+            break;
+
+            case (score >= 50 && score < 60):
+             alert ("You got a C, you can do better")
+             break;
+
+            case (score >= 40 && score < 50):
+             alert ("You got a D, you need to study more")
+            break;
+
+            case (score >= 0 && score < 40):
+             alert ("You don fail o, try again next term")
+            break;
+
+            default:
+             alert ("You entered an invalid number")
+   }
+         {
+
+            
+
+   }
+    } else {
+        alert ("You entered an invalid number")
+    }
+}
+
+
+checkerWithSwitch()
+
+
